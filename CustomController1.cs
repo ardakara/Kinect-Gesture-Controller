47ff0b00f1247d60b827b0c46d5a2505ffe08e71
Arda Kara <ardakara@gmail.com>
2012-01-22 18:08:29 -0800

    third gesture in progress

--- CustomController1.cs
+++ CustomController1.cs
@@ -37,6 +37,15 @@ namespace SkeletalTracking
                 targets[2].setTargetUnselected();
             }
 
+            if (isCloseToBody(skeleton))
+            {
+                targets[3].setTargetSelected();
+            }
+            else
+            {
+                targets[3].setTargetUnselected();
+            }
+
         }
 
         public override void controllerActivated(Dictionary<int, Target> targets)
@@ -82,11 +91,36 @@ namespace SkeletalTracking
             double deltaY = Math.Max(Math.Abs(leftHand.Position.Y - centerShoulder.Position.Y), Math.Abs(rightHand.Position.Y - centerShoulder.Position.Y));
             double deltaZ = Math.Max(Math.Abs(leftHand.Position.Z - centerShoulder.Position.Z), Math.Abs(rightHand.Position.Z - centerShoulder.Position.Z));
 
-            const int minimumArmDistance = 75;
+            const int minimumArmDistance = 65;
 
             double armDistance = getDistance(deltaX, deltaY, deltaZ);
 
             if (armDistance > minimumArmDistance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool isCloseToBody(SkeletonData skeleton)
+        {
+            Joint centerHip = skeleton.Joints[JointID.HipCenter].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+
+            Joint leftHand = skeleton.Joints[JointID.HandLeft].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+            Joint rightHand = skeleton.Joints[JointID.HandRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+
+            double deltaX = Math.Min(Math.Abs(leftHand.Position.X - centerHip.Position.X), Math.Abs(rightHand.Position.X - centerHip.Position.X));
+            double deltaY = Math.Min(Math.Abs(leftHand.Position.Y - centerHip.Position.Y), Math.Abs(rightHand.Position.Y - centerHip.Position.Y));
+            double deltaZ = Math.Min(Math.Abs(leftHand.Position.Z - centerHip.Position.Z), Math.Abs(rightHand.Position.Z - centerHip.Position.Z));
+
+            const int minimumBodyDistance = 30;
+
+            double bodyDistance = getDistance(deltaX, deltaY, deltaZ);
+
+            if (bodyDistance < minimumBodyDistance)
             {
                 return true;
             }
