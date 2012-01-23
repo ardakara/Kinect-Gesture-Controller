4e7bb8ae8ad0e0e6b96f32c945d863065b21a494
Arda Kara <ardakara@gmail.com>
2012-01-22 17:57:09 -0800

    gestures first take

--- CustomController1.cs
+++ CustomController1.cs
@@ -19,6 +19,23 @@ namespace SkeletalTracking
         {
 
             /* YOUR CODE HERE*/
+            if (areWristsTogether(skeleton))
+            {
+                targets[1].setTargetSelected();
+            }
+            else
+            {
+                targets[1].setTargetUnselected();
+            }
+
+            if (areArmsStraight(skeleton))
+            {
+                targets[2].setTargetSelected();
+            }
+            else
+            {
+                targets[2].setTargetUnselected();
+            }
 
         }
 
@@ -27,6 +44,61 @@ namespace SkeletalTracking
 
             /* YOUR CODE HERE */
 
+        }
+
+        private bool areWristsTogether(SkeletonData skeleton)
+        {
+            Joint leftWrist = skeleton.Joints[JointID.WristLeft].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+            Joint rightWrist = skeleton.Joints[JointID.WristRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+
+            Joint leftHand = skeleton.Joints[JointID.HandLeft].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+            Joint rightHand = skeleton.Joints[JointID.HandRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+
+            double deltaX = Math.Min(Math.Abs(leftWrist.Position.X - rightWrist.Position.X), Math.Abs(leftHand.Position.X - rightHand.Position.X));
+            double deltaY = Math.Min(Math.Abs(leftWrist.Position.Y - rightWrist.Position.Y), Math.Abs(leftHand.Position.Y - rightHand.Position.Y));
+            double deltaZ = Math.Min(Math.Abs(leftWrist.Position.Z - rightWrist.Position.Z), Math.Abs(leftHand.Position.Z - rightHand.Position.Z));
+
+            const int maximumWristDistance = 50;
+            if (deltaX < maximumWristDistance &&
+                deltaY < maximumWristDistance &&
+                deltaZ < maximumWristDistance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool areArmsStraight(SkeletonData skeleton)
+        {
+            Joint centerShoulder = skeleton.Joints[JointID.ShoulderCenter].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+            
+            Joint leftHand = skeleton.Joints[JointID.HandLeft].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+            Joint rightHand = skeleton.Joints[JointID.HandRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+
+            double deltaX = Math.Max(Math.Abs(leftHand.Position.X - centerShoulder.Position.X), Math.Abs(rightHand.Position.X - centerShoulder.Position.X));
+            double deltaY = Math.Max(Math.Abs(leftHand.Position.Y - centerShoulder.Position.Y), Math.Abs(rightHand.Position.Y - centerShoulder.Position.Y));
+            double deltaZ = Math.Max(Math.Abs(leftHand.Position.Z - centerShoulder.Position.Z), Math.Abs(rightHand.Position.Z - centerShoulder.Position.Z));
+
+            const int minimumArmDistance = 75;
+
+            double armDistance = getDistance(deltaX, deltaY, deltaZ);
+
+            if (armDistance > minimumArmDistance)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private double getDistance(double x, double y, double z)
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
         }
     }
 }
