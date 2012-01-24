000c3f1e57b20b8317b7b47afbae63db125a2846
Charlton Soesanto <charltonsoesanto@gmail.com>
2012-01-24 14:23:53 -0800

    Brightness works--still one bug with losing color at limit

--- CustomController2.cs
+++ CustomController2.cs
@@ -14,21 +14,22 @@ namespace SkeletalTracking
     class CustomController2 : SkeletonController
     {
 
-        byte background_red;
-        byte background_blue;
-        byte background_green;
-        byte alpha;
-        int left_circle_id;
+        double[] argb;
+        double[] bright_rgb;
+        double[] hsv;
         double[] thetas;
+        int left_circle_id;
         int right_circle_id;
 
         public CustomController2(MainWindow win) : base(win)
         {
-            background_red = 0;
-            background_blue = 0;
-            background_green = 0;
-            alpha = 127;
+
+            argb = new double[] { 0.5, 0.5, 0.5, 0.5};
+            hsv = new double[3];
+            bright_rgb = new double[] { 0.5, 0.5, 0.5, 0.5 };
+
             left_circle_id = -1;
+            
             thetas = new double[] { -1, -1 };
             right_circle_id = -1;
         }
@@ -43,6 +44,15 @@ namespace SkeletalTracking
             trackHand(rightHand, 1, targets);
         }
 
+        /*This function takes in a RGB value [0,1] and returns a byte [0, 255]*/
+        private byte doubleToByte(double d)
+        {
+            
+            byte byte_val = (byte)((int)(d * 255));
+            Console.WriteLine("d: " + d + "byte: " + byte_val);
+            return byte_val;
+        }
+
         private double trackHand(Joint hand, int lr, Dictionary<int, Target> targets) {
             bool inCircle = false;
             for (int i = 1; i <= 4; i++)
@@ -54,8 +64,8 @@ namespace SkeletalTracking
                     inCircle = true;
                     int change = increaseOrDecrease(hand, lr, cur);
                     Console.WriteLine(change);
-                    adjust_circle_color(i, change, cur);
-                    targets[5].setTargetColor(Color.FromArgb(alpha, background_red, background_green, background_blue));
+                    adjust_circles_color(i, change, targets);
+                    targets[5].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), doubleToByte(argb[1]), doubleToByte(argb[2]), doubleToByte(argb[3])));
                 }
             }
             if (!inCircle)
@@ -106,57 +116,156 @@ namespace SkeletalTracking
             }
         }
 
-        private void adjust_circle_color(int i, int change, Target cur) {
-            int colorDelta;
+        private void adjust_circles_color(int i, int change, Dictionary<int, Target> targets)
+        {
+            double colorDelta;
             if (change < 0)
             {
-                colorDelta = -2;
+                colorDelta = -0.01;
             }
             else
             {
-                colorDelta = 2;
+                colorDelta = 0.01;
             }
             switch (i) {
                 case 1:
-                    background_red = change_color(background_red, colorDelta);
-                    cur.setTargetColor(Color.FromArgb(alpha, background_red, 0, 0));
+                    argb[i] = change_color(argb, i, colorDelta);
+                    targets[i].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), doubleToByte(argb[1]), 0, 0));
                     break;
                 case 2:
-                    background_green = change_color(background_green, colorDelta);
-                    cur.setTargetColor(Color.FromArgb(alpha, 0, background_green, 0));
+                    argb[i] = change_color(argb, i, colorDelta);
+                    targets[i].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), 0, doubleToByte(argb[2]), 0));
                     break;
                 case 3:
-                    background_blue = change_color(background_blue, colorDelta);
-                    cur.setTargetColor(Color.FromArgb(alpha, 0, 0, background_blue));
+                    argb[i] = change_color(argb, i, colorDelta);
+                    targets[i].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), 0, 0, doubleToByte(argb[3]) ) );
                     break;
-                case 4: 
-                    scale_brightness();
+                case 4:
+                    scale_brightness(colorDelta);
+                    targets[1].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), doubleToByte(argb[1]), 0, 0));
+                    targets[2].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), 0, doubleToByte(argb[2]), 0));
+                    targets[3].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), 0, 0, doubleToByte(argb[3])));
                     break;
             }
+            RGBtoHSV(hsv, argb[1], argb[2], argb[3]);
+            HSVtoRGB(bright_rgb, hsv[1], 0, hsv[2]);
+            targets[4].setTargetColor(Color.FromArgb(doubleToByte(argb[0]), doubleToByte(bright_rgb[1]), doubleToByte(bright_rgb[2]), doubleToByte(bright_rgb[3])));
         }
 
-        private byte change_color(byte orig_color, int colorDelta)
+        private void scale_brightness(double colorDelta)
         {
-            if ((int)orig_color + colorDelta > (int)255)
+            RGBtoHSV(hsv, argb[1], argb[2], argb[3]);
+            hsv[2] = change_color(hsv, 2, colorDelta);
+            HSVtoRGB(argb, hsv[0], hsv[1], hsv[2]);
+        }
+
+        private double change_color(double[] array, int i, double colorDelta)
+        {
+            if (array[i] + colorDelta > 1)
             {
-                return 255;
-            } else if ((int)orig_color + colorDelta < (int)0)
+                return 1;
+            }
+            else if (array[i] + colorDelta < 0)
             {
                 return 0;
             }
             else
             {
-                return (byte)(orig_color + colorDelta);
+                return array[i] + colorDelta;
             }
         }
 
-        private void scale_brightness()
+
+        private void RGBtoHSV(double[] hsv, double r, double g, double b)
         {
-            background_red /= 2;
-            background_green /= 2;
-            background_blue /= 2;
+            double min, max, delta;
+            min = Math.Min( Math.Min(r, g), Math.Min(g, b));
+            max = Math.Max(Math.Max(r, g), Math.Max(g, b));
+            delta = max - min;
+
+            hsv[2] = max; //set v
+            if (max != 0)
+            {
+                hsv[1]= delta / max; //set s
+            }
+            else
+            {
+                hsv[1] = 0;
+                hsv[0] = -1;
+                return;
+            }
+
+            if (r == max)
+            {
+                hsv[0] = (g - b) / delta;
+            }
+            else if (g == max)
+            {
+                hsv[0] = 2 + (b - r) / delta;
+            }
+            else
+            {
+                hsv[0] = 4 + (r - g) / delta;
+            }
+
+            hsv[0] *= 60;
+            if (hsv[0] < 0) {
+                hsv[0] += 360;
+            }
+
         }
 
+        private void HSVtoRGB(double[] argb, double h, double s, double v)
+        {
+            int i;
+            double f, p, q, t;
+            if (s == 0) //achromatic (grey)
+            {
+                argb[1] = argb[2] = argb[3] = v;
+                return;
+            }
+
+            h /= 60;
+            i = (int) Math.Floor(h);
+            f = h - i;
+            p = v * (1 - s);
+            q = v * (1 - s * f);
+            t = v * (1 - s * (1 - f));
+
+            switch (i)
+            {
+                case 0:
+                    argb[1] = v;
+                    argb[2] = t;
+                    argb[3] = p;
+                    break;
+                case 1:
+                    argb[1] = q;
+                    argb[2] = v;
+                    argb[3] = p;
+                    break;
+                case 2:
+                    argb[1] = p;
+                    argb[2] = v;
+                    argb[3] = t;
+                    break;
+                case 3:
+                    argb[1] = p;
+                    argb[2] = q;
+                    argb[3] = v;
+                    break;
+                case 4:
+                    argb[1] = t;
+                    argb[2] = p;
+                    argb[3] = v;
+                    break;
+                default:
+                    argb[1] = v;
+                    argb[2] = p;
+                    argb[3] = q;
+                    break;
+            }
+        }
         private double distance(double x1, double y1, double x2, double y2)
         {
             return Math.Sqrt(Math.Pow(Math.Abs(x1 - x2), 2) + Math.Pow(Math.Abs(y1 - y2), 2));
@@ -165,11 +274,11 @@ namespace SkeletalTracking
         public override void controllerActivated(Dictionary<int, Target> targets)
         {
             //1 is RED, 2 is GREEN, 3 is BLUE, 4 is BRIGHTNESS
-            targets[1].setTargetColor(Color.FromArgb(127, background_red, 0, 0));
-            targets[2].setTargetColor(Color.FromArgb(127, 0, background_green, 0));
-            targets[3].setTargetColor(Color.FromArgb(127, 0, 0, background_blue));
+            targets[1].setTargetColor(Color.FromArgb(127, doubleToByte(argb[1]), 0, 0));
+            targets[2].setTargetColor(Color.FromArgb(127, 0, doubleToByte(argb[2]), 0));
+            targets[3].setTargetColor(Color.FromArgb(127, 0, 0, doubleToByte(argb[3])));
             targets[4].setTargetColor(Color.FromArgb(127, 127, 127, 127));
-            targets[5].setTargetColor(Color.FromArgb(127, background_red, background_green, background_blue));
+            targets[5].setTargetColor(Color.FromArgb(127, doubleToByte(argb[1]), doubleToByte(argb[2]), doubleToByte(argb[3]) ));
         }
     }
 
